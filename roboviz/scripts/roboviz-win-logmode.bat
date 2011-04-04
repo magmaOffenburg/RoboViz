8fe7c5fe3bcf5e7db6d0de514fb958466bef740d
Justin Stoecker <justin@cs.miami.edu>
2011-04-04 14:17:23 +0000

    Increased JVM initial/max heap size when replaying logfiles.
    git-svn-id: http://svn.code.sf.net/p/rcroboviz/code/trunk@14 9d517d49-3bcd-4e79-a2cd-a7844ee3de28

--- roboviz/scripts/roboviz-win-logmode.bat
+++ roboviz/scripts/roboviz-win-logmode.bat
@@ -1,3 +1,3 @@
 @echo off
 set LOGFILE=
-java -Djava.library.path=lib -cp lib\jogl.all.jar;lib\nativewindow.all.jar;lib\gluegen-rt.jar;lib\newt.all.jar;lib\jsgl.jar;. rv.Viewer --logfile %LOGFILE%
+java -Xms256m -Xmx512m -Djava.library.path=lib -cp lib\jogl.all.jar;lib\nativewindow.all.jar;lib\gluegen-rt.jar;lib\newt.all.jar;lib\jsgl.jar;. rv.Viewer --logfile %LOGFILE%
--- roboviz/scripts/roboviz.sh
+++ roboviz/scripts/roboviz.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" == --logfile ]; then
     echo "Starting RoboViz in logfile mode"
-    java -Djava.library.path=lib/ -cp $CLASSPATH:lib/jogl.all.jar:lib/nativewindow.all.jar:lib/gluegen-rt.jar:lib/newt.all.jar:lib/jsgl.jar:. rv.Viewer --logfile $2
+    java -Xms256m -Xmx512m -Djava.library.path=lib/ -cp $CLASSPATH:lib/jogl.all.jar:lib/nativewindow.all.jar:lib/gluegen-rt.jar:lib/newt.all.jar:lib/jsgl.jar:. rv.Viewer --logfile $2
     
 else
     echo "Starting RoboViz in live mode"
