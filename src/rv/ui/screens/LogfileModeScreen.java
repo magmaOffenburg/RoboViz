ebcd9e2fb1853285b7cb7356eba97d624a68d3cc
Jens Fischer <jensfischer95@gmail.com>
2015-05-16 14:31:40 +0200

    Move LogPlayerOverlay logic to LogfileModeScreen

--- src/rv/ui/screens/LogfileModeScreen.java
+++ src/rv/ui/screens/LogfileModeScreen.java
@@ -16,18 +16,65 @@
 
 package rv.ui.screens;
 
+import java.awt.event.KeyEvent;
+import javax.media.opengl.awt.GLCanvas;
 import rv.Viewer;
+import rv.comm.rcssserver.LogPlayer;
 
 public class LogfileModeScreen extends ViewerScreenBase {
 
+    private final LogPlayer      player;
+    private final PlayerControls playDialog;
+
     public LogfileModeScreen(Viewer viewer) {
         super(viewer);
-        overlays.add(new LogPlayerOverlay(viewer));
+        this.player = viewer.getLogPlayer();
+        playDialog = PlayerControls.getInstance(player);
+    }
+
+    @Override
+    public void setEnabled(GLCanvas canvas, boolean enabled) {
+        super.setEnabled(canvas, enabled);
+        if (enabled) {
+            playDialog.showFrame(viewer.getFrame());
+        } else {
+            playDialog.hideFrame(viewer.getFrame());
+        }
     }
 
     @Override
     protected void addTextOverlay(TextOverlay textOverlay) {
         super.addTextOverlay(textOverlay);
         textOverlay.setTimeScale(viewer.getLogPlayer().getPlayBackSpeed());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        super.keyPressed(e);
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_P:
+            if (player.isPlaying())
+                player.pause();
+            else
+                player.resume();
+            break;
+        case KeyEvent.VK_R:
+            player.rewind();
+            break;
+        case KeyEvent.VK_X:
+            player.decreasePlayBackSpeed();
+            break;
+        case KeyEvent.VK_C:
+            player.increasePlayBackSpeed();
+            break;
+        case KeyEvent.VK_COMMA:
+            if (!player.isPlaying())
+                player.stepBackward();
+            break;
+        case KeyEvent.VK_PERIOD:
+            if (!player.isPlaying())
+                player.stepForward();
+            break;
+        }
     }
 }
