magmaOffenburg/RoboViz

commit c343ad64b37e59b07b30f54a9b220941fe5fb221

Log mode: reset the WorldModel when changing log files
This prevents old state from sticking around, most notably the team colors and names on the GameStateOverlay and the scene graph from the last frame of the previous log.

src/rv/comm/rcssserver/GameState.java
@@ -189,6 +189,16 @@ public class GameState implements ServerChangeListener {
         listeners.remove(l);
     }
 
+    public void reset() {
+        teamLeft = null;
+        teamRight = null;
+        scoreLeft = 0;
+        scoreRight = 0;
+        playMode = "<Play Mode>";
+        time = 0;
+        half = 0;
+    }
+
     /**
      * Parses expression and updates state
      */

src/rv/world/WorldModel.java
@@ -54,6 +54,8 @@ public class WorldModel {
 
     private final GameState                          gameState    = new GameState();
     private SceneGraph                               sceneGraph   = null;
+    private ContentManager                           cm;
+    private Configuration                            config;
 
     private final ArrayList<ISceneGraphItem>         sgItems      = new ArrayList<>();
 
@@ -165,18 +167,15 @@ public class WorldModel {
     }
 
     public void init(GL glObj, ContentManager cm, Configuration config, Viewer.Mode mode) {
+        this.cm = cm;
+        this.config = config;
         GL2 gl = glObj.getGL2();
 
         field = new Field(cm.getModel("models/newfield.obj"), cm);
         gameState.addListener(field);
         gameState.addListener(cm);
 
-        leftTeam = new Team(new float[] { .15f, .15f, 1.0f, 1.0f }, Team.LEFT, cm,
-                config.teamColors);
-        gameState.addListener(leftTeam);
-        rightTeam = new Team(new float[] { 1.0f, .15f, .15f, 1.0f }, Team.RIGHT, cm,
-                config.teamColors);
-        gameState.addListener(rightTeam);
+        initTeams();
 
         sgItems.add(leftTeam);
         sgItems.add(rightTeam);
@@ -197,6 +196,15 @@ public class WorldModel {
         lighting.addLight(d1);
     }
 
+    private void initTeams() {
+        leftTeam = new Team(new float[] { .15f, .15f, 1.0f, 1.0f }, Team.LEFT, cm,
+                config.teamColors);
+        gameState.addListener(leftTeam);
+        rightTeam = new Team(new float[] { 1.0f, .15f, .15f, 1.0f }, Team.RIGHT, cm,
+                config.teamColors);
+        gameState.addListener(rightTeam);
+    }
+
     public void update(GL gl, double elapsedMS, UserInterface ui) {
         // Allow scene graph items to update their states prior to rendering.
         // This is done in the update loop rather than the scene graph update
@@ -213,5 +221,12 @@ public class WorldModel {
     public void dispose(GL gl) {
         if (field != null)
             field.dispose(gl);
+    }
+
+    public void reset() {
+        gameState.reset();
+        if (cm != null && config != null)
+            initTeams();
+        setSceneGraph(null);
     }
 }

src/rv/ui/screens/LogfileModeScreen.java
@@ -29,7 +29,7 @@ public class LogfileModeScreen extends ViewerScreenBase {
     private final PlayerControls playDialog;
     private final InfoOverlay    openFileOverlay;
 
-    public LogfileModeScreen(Viewer viewer) {
+    public LogfileModeScreen(final Viewer viewer) {
         super(viewer);
         this.player = viewer.getLogPlayer();
         playDialog = PlayerControls.getInstance(player);
@@ -45,6 +45,7 @@ public class LogfileModeScreen extends ViewerScreenBase {
             public void logfileChanged() {
                 prevScoreL = -1;
                 prevScoreR = -1;
+                viewer.getWorldModel().reset();
             }
         });
     }

commit 2d3595f2bc76bb2d47ac44db08a352f4e6381bc9

Fix broken agent localization after opening a different logfile
This issue was introduced in c343ad6.

src/rv/comm/rcssserver/GameState.java
@@ -16,7 +16,6 @@
 
 package rv.comm.rcssserver;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import rv.comm.rcssserver.ServerComm.ServerChangeListener;

src/rv/comm/rcssserver/LogPlayer.java
@@ -19,7 +19,6 @@ package rv.comm.rcssserver;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import javax.swing.JFileChooser;

src/rv/world/Team.java
@@ -16,7 +16,6 @@
 
 package rv.world;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import js.jogl.model.ObjMaterial;

src/rv/world/WorldModel.java
@@ -177,9 +177,6 @@ public class WorldModel {
 
         initTeams();
 
-        sgItems.add(leftTeam);
-        sgItems.add(rightTeam);
-
         ball = new Ball(cm);
         sgItems.add(ball);
 
@@ -197,12 +194,20 @@ public class WorldModel {
     }
 
     private void initTeams() {
+        if (leftTeam != null)
+            sgItems.remove(leftTeam);
+        if (rightTeam != null)
+            sgItems.remove(rightTeam);
+
         leftTeam = new Team(new float[] { .15f, .15f, 1.0f, 1.0f }, Team.LEFT, cm,
                 config.teamColors);
         gameState.addListener(leftTeam);
         rightTeam = new Team(new float[] { 1.0f, .15f, .15f, 1.0f }, Team.RIGHT, cm,
                 config.teamColors);
         gameState.addListener(rightTeam);
+
+        sgItems.add(leftTeam);
+        sgItems.add(rightTeam);
     }
 
     public void update(GL gl, double elapsedMS, UserInterface ui) {