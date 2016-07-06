magmaOffenburg/RoboViz

commit eb7eaf47528d9d7904fb95aac1059f409ca9afdf

Fix connection overlay not showing when losing connection while in playmode overlay
Also fixed a ConcurrentModificationException in ServerComm.

src/rv/ui/screens/PlaymodeOverlay.java
@@ -171,7 +171,6 @@ public class PlaymodeOverlay extends ScreenBase implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
-
     }
 
     @Override

src/rv/comm/rcssserver/ServerComm.java
@@ -26,11 +26,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.CopyOnWriteArrayList;
 import javax.swing.Timer;
 import js.math.vector.Vec3f;
 import rv.Configuration;
@@ -105,7 +105,7 @@ public class ServerComm implements DrawCommListener {
         void connectionChanged(ServerComm server);
     }
 
-    private final List<ServerChangeListener> changeListeners  = new ArrayList<>();
+    private final List<ServerChangeListener> changeListeners  = new CopyOnWriteArrayList<>();
     private Timer                            autoConnectTimer;
 
     private Socket                           socket;

src/rv/ui/screens/LiveGameScreen.java
@@ -40,6 +40,7 @@ public class LiveGameScreen extends ViewerScreenBase implements ServerComm.Serve
         super(viewer);
         ServerSpeedBenchmarker ssb = new ServerSpeedBenchmarker();
         viewer.getWorldModel().getGameState().addListener(ssb);
+        viewer.getNetManager().getServer().addChangeListener(this);
         gameStateOverlay.addServerSpeedBenchmarker(ssb);
         playmodeOverlay = new PlaymodeOverlay(viewer, this);
         overlays.add(playmodeOverlay);
@@ -67,16 +68,6 @@ public class LiveGameScreen extends ViewerScreenBase implements ServerComm.Serve
             return "Trying to connect to " + server + "...";
         else
             return "Press C to connect to " + server + ".";
-    }
-
-    @Override
-    public void setEnabled(GLCanvas canvas, boolean enabled) {
-        super.setEnabled(canvas, enabled);
-        if (enabled) {
-            viewer.getNetManager().getServer().addChangeListener(this);
-        } else {
-            viewer.getNetManager().getServer().removeChangeListener(this);
-        }
     }
 
     @Override
@@ -224,6 +215,7 @@ public class LiveGameScreen extends ViewerScreenBase implements ServerComm.Serve
         if (server.isConnected()) {
             viewer.getWorldModel().setSelectedObject(viewer.getWorldModel().getBall());
         } else {
+            playmodeOverlay.setVisible(false);
             prevScoreL = -1;
             prevScoreR = -1;
         }

commit ab4537f8ed03929bf03bd4181bf839dc282c2816

PlaymodeOverlay: prevent multiple key listeners from being registered
Caused by eb7eaf4.

src/rv/ui/screens/PlaymodeOverlay.java
@@ -59,6 +59,9 @@ public class PlaymodeOverlay extends ScreenBase implements KeyListener {
 
     @Override
     public void setVisible(boolean visible) {
+        if (visible == this.visible)
+            return;
+
         super.setVisible(visible);
         if (visible) {
             if (viewer.getWorldModel().getGameState() != null)