magmaOffenburg/RoboViz

commit 917616f6b15552227e189b6626c8e8c9e0fc38fe

Fix auto-connect blocking the UI thread every second
This led to the Swing UI (menu bar, dialogs) becoming very unresponsive when not currently connected to a server and auto-connect is enabled.

src/rv/comm/rcssserver/ServerComm.java
@@ -16,8 +16,6 @@
 
 package rv.comm.rcssserver;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.File;
@@ -55,10 +53,30 @@ public class ServerComm implements DrawCommListener
 	{
 		private final MessageParser parser = new MessageParser(world);
 
+		private final String host;
+
+		private final int port;
+
+		public MessageReceiver(String host, int port)
+		{
+			this.host = host;
+			this.port = port;
+		}
+
 		@Override
 		public void run()
 		{
 			try {
+				socket = new Socket(host, port);
+				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new DataInputStream(socket.getInputStream());
+
+				if (autoConnectTimer != null)
+					autoConnectTimer.stop();
+				setConnected(true);
+				if (recordLogs)
+					setupNewLogfile();
+
 				String message;
 				do {
 					message = readMessage();
@@ -226,19 +244,7 @@ public class ServerComm implements DrawCommListener
 
 	public void connect(String host, int port)
 	{
-		try {
-			socket = new Socket(host, port);
-			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new DataInputStream(socket.getInputStream());
-			MessageReceiver inThread = new MessageReceiver();
-			inThread.start();
-			if (autoConnectTimer != null)
-				autoConnectTimer.stop();
-			setConnected(true);
-			if (recordLogs)
-				setupNewLogfile();
-		} catch (IOException e) {
-		}
+		new MessageReceiver(host, port).start();
 	}
 
 	public void disconnect()

commit 38076e5a95b5673da79d9390d82992f1a0c8a81e

Fix concurrency issues introduced by 917616f
Have to make sure the world model isn't changed by another thread while it's being rendered / updated / ...

src/rv/Renderer.java
@@ -142,35 +142,38 @@ public class Renderer implements WindowResizeListener
 
 	public void render(GLAutoDrawable drawable, Configuration.Graphics config)
 	{
-		GL2 gl = drawable.getGL().getGL2();
+		synchronized (viewer.getWorldModel())
+		{
+			GL2 gl = drawable.getGL().getGL2();
 
-		if (config.useShadows) {
-			ShadowMapRenderer shadowRenderer = effectManager.getShadowRenderer();
-			shadowRenderer.render(gl, viewer.getWorldModel(), viewer.getDrawings());
-		}
+			if (config.useShadows) {
+				ShadowMapRenderer shadowRenderer = effectManager.getShadowRenderer();
+				shadowRenderer.render(gl, viewer.getWorldModel(), viewer.getDrawings());
+			}
 
-		if (graphics.useStereo) {
-			vantage.applyLeft(gl, glu, viewer.getScreen());
-			gl.glDrawBuffer(GL2.GL_BACK_LEFT);
-			gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-			drawScene(gl);
+			if (graphics.useStereo) {
+				vantage.applyLeft(gl, glu, viewer.getScreen());
+				gl.glDrawBuffer(GL2.GL_BACK_LEFT);
+				gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+				drawScene(gl);
 
-			vantage.applyRight(gl, glu, viewer.getScreen());
-			gl.glDrawBuffer(GL2.GL_BACK_RIGHT);
-			gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-			drawScene(gl);
+				vantage.applyRight(gl, glu, viewer.getScreen());
+				gl.glDrawBuffer(GL2.GL_BACK_RIGHT);
+				gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+				drawScene(gl);
 
-			gl.glDrawBuffer(GL.GL_BACK);
-			viewer.getUI().render(gl, glu, glut);
-		} else {
-			gl.glDrawBuffer(GL.GL_BACK);
-			gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+				gl.glDrawBuffer(GL.GL_BACK);
+				viewer.getUI().render(gl, glu, glut);
+			} else {
+				gl.glDrawBuffer(GL.GL_BACK);
+				gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 
-			vantage.apply(gl, glu, viewer.getScreen());
+				vantage.apply(gl, glu, viewer.getScreen());
 
-			drawScene(gl);
+				drawScene(gl);
 
-			viewer.getUI().render(gl, glu, glut);
+				viewer.getUI().render(gl, glu, glut);
+			}
 		}
 	}
 

src/rv/comm/rcssserver/MessageParser.java
@@ -44,17 +44,20 @@ public class MessageParser
 
 	public void parse(String message) throws ParseException
 	{
-		ArrayList<SExp> expressions = SExp.parse(message);
+		synchronized (world)
+		{
+			ArrayList<SExp> expressions = SExp.parse(message);
 
-		world.getGameState().parse(expressions.get(0), world);
-		SceneGraphHeader header = SceneGraphHeader.parse(expressions.get(1));
-		if (header.getType().equals(SceneGraphHeader.FULL)) {
-			// scene graph structure has changed, so replace the old one and tell
-			// any objects that rely on the scene graph to update their references
-			SceneGraph sg = new SceneGraph(expressions.get(2));
-			world.setSceneGraph(sg);
-		} else {
-			world.getSceneGraph().update(expressions.get(2));
+			world.getGameState().parse(expressions.get(0), world);
+			SceneGraphHeader header = SceneGraphHeader.parse(expressions.get(1));
+			if (header.getType().equals(SceneGraphHeader.FULL)) {
+				// scene graph structure has changed, so replace the old one and tell
+				// any objects that rely on the scene graph to update their references
+				SceneGraph sg = new SceneGraph(expressions.get(2));
+				world.setSceneGraph(sg);
+			} else {
+				world.getSceneGraph().update(expressions.get(2));
+			}
 		}
 	}
 }

src/rv/comm/rcssserver/ServerComm.java
@@ -71,8 +71,6 @@ public class ServerComm implements DrawCommListener
 				out = new PrintWriter(socket.getOutputStream(), true);
 				in = new DataInputStream(socket.getInputStream());
 
-				if (autoConnectTimer != null)
-					autoConnectTimer.stop();
 				setConnected(true);
 				if (recordLogs)
 					setupNewLogfile();
@@ -244,6 +242,8 @@ public class ServerComm implements DrawCommListener
 
 	public void connect(String host, int port)
 	{
+		if (autoConnectTimer != null)
+			autoConnectTimer.stop();
 		new MessageReceiver(host, port).start();
 	}
 

src/rv/world/WorldModel.java
@@ -130,7 +130,7 @@ public class WorldModel
 		return sceneGraph;
 	}
 
-	public void setSceneGraph(SceneGraph sceneGraph)
+	public synchronized void setSceneGraph(SceneGraph sceneGraph)
 	{
 		this.sceneGraph = sceneGraph;
 
@@ -233,7 +233,7 @@ public class WorldModel
 		sgItems.add(rightTeam);
 	}
 
-	public void update(GL gl, double elapsedMS, UserInterface ui)
+	public synchronized void update(GL gl, double elapsedMS, UserInterface ui)
 	{
 		// Allow scene graph items to update their states prior to rendering.
 		// This is done in the update loop rather than the scene graph update
@@ -316,7 +316,7 @@ public class WorldModel
 			field.dispose(gl);
 	}
 
-	public void reset()
+	public synchronized void reset()
 	{
 		gameState.reset();
 		if (cm != null && config != null)