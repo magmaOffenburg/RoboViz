magmaOffenburg/RoboViz

commit fe58279bb88ee1cf360ef4989b55e993256b3526

fix team color not working & fix crash when saving failed

src/main/kotlin/org/magmaoffenburg/roboviz/configuration/Config.kt
@@ -143,7 +143,7 @@ class Config(args: Array<String>) {
 
         // TeamColors
         parser.getValueSuperKey("Team Color").forEach {
-            TeamColors.byTeamNames[it.key] = Color(Integer.decode(it.value))
+            TeamColors.byTeamNames[it.key.substringAfter(":").trim()] = Color(Integer.decode(it.value))
         }
 
         // Args
@@ -226,7 +226,12 @@ class Config(args: Array<String>) {
             }
         }
 
-        parser.writeFile(filePath)
+        try {
+            parser.writeFile(filePath)
+        } catch (e: Exception) {
+            logger.error { "Error while trying to save the config: ${e.printStackTrace()}" }
+        }
+
     }
 
     fun configChanged() {

commit 6dd4dd6a204d0f8b945228c4395f2ce8aa7caea6

fix team color saving
regression from fe58279

src/main/java/rv/content/ContentManager.java
@@ -300,12 +300,14 @@ public class ContentManager implements SceneGraphListener, GameState.GameStateCh
 		// if team name changed, update the materials
 		String teamNameLeft = gs.getTeamLeft();
 		if (!Objects.equals(teamNameLeft, this.teamNameLeft)) {
-			updateTeamColor(teamNameLeft, "matLeft", config.getByTeamNames().get("<Left>"));
+			updateTeamColor(teamNameLeft, "matLeft",
+					config.getByTeamNames().getOrDefault("<Left>", config.getDefaultLeft()));
 			this.teamNameLeft = teamNameLeft;
 		}
 		String teamNameRight = gs.getTeamRight();
 		if (!Objects.equals(teamNameRight, this.teamNameRight)) {
-			updateTeamColor(teamNameRight, "matRight", config.getByTeamNames().get("<Right>"));
+			updateTeamColor(teamNameRight, "matRight",
+					config.getByTeamNames().getOrDefault("<Right>", config.getDefaultRight()));
 			this.teamNameRight = teamNameRight;
 		}
 	}

src/main/java/rv/world/WorldModel.java
@@ -222,9 +222,11 @@ public class WorldModel
 		}
 
 		TeamColors teamColors = TeamColors.INSTANCE;
-		leftTeam = new Team(teamColors.getByTeamNames().get("<Left>"), Team.LEFT, cm, teamColors);
+		leftTeam = new Team(teamColors.getByTeamNames().getOrDefault("<Left>", teamColors.getDefaultLeft())
+				, Team.LEFT, cm, teamColors);
 		gameState.addListener(leftTeam);
-		rightTeam = new Team(teamColors.getByTeamNames().get("<Right>"), Team.RIGHT, cm, teamColors);
+		rightTeam = new Team(teamColors.getByTeamNames().getOrDefault("<Right>", teamColors.getDefaultRight())
+				, Team.RIGHT, cm, teamColors);
 		gameState.addListener(rightTeam);
 
 		sgItems.add(leftTeam);

src/main/kotlin/org/magmaoffenburg/roboviz/configuration/Config.kt
@@ -220,18 +220,18 @@ class Config(args: Array<String>) {
 
         // team colors
         TeamColors.byTeamNames.forEach {
-            parser.setValue(it.key, "0x${Integer.toHexString(it.value.rgb and 0xFFFFFF)}")
+            parser.setValue("Team Color : ${it.key}", "0x${Integer.toHexString(it.value.rgb and 0xFFFFFF)}")
         }
         parser.getValueSuperKey("Team Color").forEach {
-            if (!TeamColors.byTeamNames.containsKey(it.key)) {
+            if (!TeamColors.byTeamNames.containsKey(it.key.substringAfter(":").trim())) {
                 parser.removeValue(it.key) // remove deleted team color
             }
         }
 
         try {
             parser.writeFile(filePath)
-        } catch (e: Exception) {
-            logger.error { "Error while trying to save the config: ${e.printStackTrace()}" }
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            logger.error("Error while trying to save the config.", e)
         }
 
     }

src/main/kotlin/org/magmaoffenburg/roboviz/gui/windows/config/GeneralPanel.kt
@@ -160,7 +160,7 @@ class GeneralPanel: JPanel() {
         tableModel.addTableModelListener {
             TeamColors.byTeamNames.clear()
             for (i in 0 until tableModel.rowCount) {
-                val key = "Team Color : ${tableModel.getValueAt(i,0)}"
+                val key = tableModel.getValueAt(i,0).toString()
                 val color = tableModel.getValueAt(i,1) as Color
 
                 TeamColors.byTeamNames[key] = color